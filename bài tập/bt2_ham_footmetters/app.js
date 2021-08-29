2fe1cdf6503fa6cae81255afde99535ae69bc7d9
Khanh <uefa.euro30@gmail.com>
2021-08-29 14:51:09 +0700

    first commit

---
+++ bài tập/bt2_ham_footmetters/app.js
@@ -0,0 +1,12 @@
+function footToMeter(foot){
+    let meter = 0.305 * foot
+    return meter;
+}
+function meterToFoot(meter){
+    let foot = 3.279 * meter
+    return foot;
+}
+
+console.log(footToMeter(2))
+console.log(meterToFoot(20))
+
